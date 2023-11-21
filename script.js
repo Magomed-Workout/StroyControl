1cac914eac13daca551b3f7286fd6797d7f7a832
Magomed-Workout <128885397+Magomed-Workout@users.noreply.github.com>
2023-11-21 22:36:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ function onscroll(entry) {
 let options = {
     threshold: [0.5] };
 let observer = new IntersectionObserver(onscroll, options);
-let elements = document.querySelectorAll('.anim.header, .anim.section-1, .anim.section-2, .anim.section-3, .anim.section-4, .anim.section-left-5, .anim.section-right-5, .anim.section-6, .anim.title-sect-7, .anim.description-sect-7, .anim.section-left-1, .anim.section-right-1, .anim.section-left-2, .anim.section-right-2, .anim.section-left-3, .anim.section-right-3, .anim.section-8, .anim.petal, .anim.section-9, .anim.section-left-10, .anim.section-right-10, .anim.container-10, .anim.section-left-11, .anim.section-right-11, .anim.title-sect-12, .anim.description-sect-12, .anim.section-left-12, .anim.section-right-12, .anim.container-contech, .anim.section-13, .anim.footer');
+let elements = document.querySelectorAll('.anim.header, .anim.section-1, .anim.section-2, .anim.section-3, .anim.section-4, .anim.section-left-5, .anim.section-right-5, .anim.section-6, .anim.title-sect-7, .anim.description-sect-7, .anim.section-left-1, .anim.section-right-1, .anim.section-left-2, .anim.section-right-2, .anim.section-left-3, .anim.section-right-3, .anim.section-8, .anim.section-9, .anim.title-description-9, .anim.container-sect-9, .anim.cont-sect-9, .anim.section-left-10, .anim.section-right-10, .anim.container-10, .anim.section-left-11, .anim.section-right-11, .anim.title-sect-12, .anim.description-sect-12, .anim.section-left-12, .anim.section-right-12, .anim.container-contech, .anim.section-13, .anim.footer');
 
 for (let elm of elements) {
     observer.observe(elm);
